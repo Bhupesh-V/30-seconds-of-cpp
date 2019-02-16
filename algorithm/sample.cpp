51aec04c6c745ba65a7a497ad11d2ad499f2df50
Bhupesh-V <varshneybhupesh@gmail.com>
2019-02-16 20:19:35 +0530

    added code folders

---
+++ algorithm/sample.cpp
@@ -0,0 +1,52 @@
+// Follow this format to add complete code snippets
+
+/*
+Naming Style : 
+    Name your program just the same as listed in the header.md file
+*/
+
+//Comment Header:
+//Add the following comment header in every program.
+/*
+    Author : this must be your name ;)
+    Date : Date format dd/mm/yyyy
+    Time : Time format hh:mm
+    Description : description should be small & one liner.
+*/
+ 
+
+//Style Guide : Please follow the style guide for your code to be merged. 
+/*
+
+1. Add Opening braces on the same line just like below
+    int main()
+    {
+                            // DO NOT DO THIS    
+    }
+
+    int main(){
+                            // DO THIS.
+    }
+
+2. Use Tab size of 4 or 4 Spaces.Be consistent with whatever you choose
+    Use only one indenting format for the whole program.
+
+3. Add appropriate comments wherever necessary to explain the code
+    Programs wth NO Comments at all will not be merged.
+
+4. Expression should be readable, Use ONE space between different TOKENS.
+    For Example : 
+    galaxy=stars+asteroids          // DO NOT USE THIS FORMAT.
+    galaxy = stars + asteroids      // USE THIS FORMAT.
+
+5. Always add braces in a for loop even its a one liner loop
+    
+    for(int i=0;i<45;i++)
+        cout<<i<<" ";               //DO NOT DO THIS.
+    
+    for(int i=0;i<45;i++){
+        cout<<i<<" ";               //DO THIS.
+    }
+
+*/
+
