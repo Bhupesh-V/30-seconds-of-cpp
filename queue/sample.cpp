74a9add4036367bd209874f2ff229099f3312a4b
Bhupesh-V <varshneybhupesh@gmail.com>
2019-02-17 11:39:33 +0530

    Added Style Guide

--- queue/sample.cpp
+++ queue/sample.cpp
@@ -1,52 +1,8 @@
-// Follow this format to add complete code snippets
-
-/*
-Naming Style : 
-    Name your program just the same as listed in the header.md file
-*/
-
-//Comment Header:
-//Add the following comment header in every program.
+// Follow the Style Guide while submitting code PR.
+// Style Guide => https://github.com/Bhupesh-V/30-Seconds-Of-STL/blob/master/CONTRIBUTING.md/#Style Guide
 /*
     Author : this must be your name ;)
     Date : Date format dd/mm/yyyy
     Time : Time format hh:mm
     Description : description should be small & one liner.
 */
- 
-
-//Style Guide : Please follow the style guide for your code to be merged. 
-/*
-
-1. Add Opening braces on the same line just like below
-    int main()
-    {
-                            // DO NOT DO THIS    
-    }
-
-    int main(){
-                            // DO THIS.
-    }
-
-2. Use Tab size of 4 or 4 Spaces.Be consistent with whatever you choose
-    Use only one indenting format for the whole program.
-
-3. Add appropriate comments wherever necessary to explain the code
-    Programs wth NO Comments at all will not be merged.
-
-4. Expression should be readable, Use ONE space between different TOKENS.
-    For Example : 
-    galaxy=stars+asteroids          // DO NOT USE THIS FORMAT.
-    galaxy = stars + asteroids      // USE THIS FORMAT.
-
-5. Always add braces in a for loop even its a one liner loop
-    
-    for(int i=0;i<45;i++)
-        cout<<i<<" ";               //DO NOT DO THIS.
-    
-    for(int i=0;i<45;i++){
-        cout<<i<<" ";               //DO THIS.
-    }
-
-*/
-
