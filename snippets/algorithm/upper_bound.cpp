1c5ebc29f8d8d0416bb98ff4d3e91e960da00883
thamara <tkcandrade@gmail.com>
2019-09-03 20:18:47 -0300

    Fixing upper_bound.cpp to reflect the code um upper_bound.md

--- snippets/algorithm/upper_bound.cpp
+++ snippets/algorithm/upper_bound.cpp
@@ -6,15 +6,56 @@
 */
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include<algorithm>
+#include<set>
 
-int main() {
-    std::vector<int> vec = {3, -1, 2, 5, 10, 6, 7, 4};
-    // upper_bound requires a sorted input {-1, 2, 3, 4, 5, 6, 7, 10}
-    std::sort(vec.begin(), vec.end());
-
-    auto upperBoundIt = std::upper_bound(vec.begin(), vec.end(), 5); 
-    // prints 6 (first value element that is greater than 5 is 6)
-    std::cout << "The lower bound element is " << *upperBoundIt << '\n';
-}
+ void show(int a[], int arraysize) { 
+        for (int i = 0; i < arraysize; ++i) { 
+            std::cout << a[i] << " "; 
+	}
+    } 
+  
+int main() { 
+    int a[] = { 5, 6, 7, 7, 6, 5, 5, 6 , 8 }; 
+    int asize = sizeof(a) / sizeof(a[0]); 
+    std::cout << "\n The array is : "; 
+    show(a, asize); 
+   
+    std::cout << "\n So, we first sort the array"; 
+    std::sort(a, a + asize); 
+    std::cout << "\n\n The array after sorting is : "; 
+    show(a, asize); 
+    std::cout << "\n\nNow, we find the upper bound on 6: "; 
+    std::cout<< std::upper_bound(a, a + asize, 6) - a << "\n";   
+    
+    
+    std::cout << "\n\nNow, we find the upper bound on 5: "; 
+    std::cout<< std::upper_bound(a, a + asize, 5) - a << "\n";
+  
+    std::cout << "\n\nNow, say we want to find upper bound for 8: "; 
+    std::cout<< std::upper_bound(a, a + asize, 8) - a << "\n \n";
+    
+     
+    std::cout<<"Now,for sets: \n\n";
+    std::set<int> s; 
+  
+    s.insert(1); 
+    s.insert(4); 
+    s.insert(2); 
+    s.insert(5); 
+    s.insert(6); 
+  
+    std::cout << "The set elements are: "; 
+    for (auto it = s.begin(); it != s.end(); it++) 
+        std::cout << *it << " "; 
+  
+    
+    auto it = s.upper_bound(2); 
+    std::cout << "\nThe upper bound of key 2 is "; 
+    std::cout << (*it) << "\n"; 
+    it = s.upper_bound(3); 
+    std::cout << "The upper bound of key 3 is "; 
+    std::cout << (*it) << "\n";
+    
+    return 0; 
+}  
