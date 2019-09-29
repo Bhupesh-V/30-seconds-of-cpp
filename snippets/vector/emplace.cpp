2b9e04dce2a27e6a9a22d9a3c9eb2718e6e2bf47
saisriram2710 <yss271001@gmail.com>
2019-09-29 11:04:21 +0530

    Added emplace() in vectors

---
+++ snippets/vector/emplace.cpp
@@ -0,0 +1,40 @@
+/*
+    Author : Y. Sai Sriram
+    Date   : 29/09/2019
+    Time   : 10:53
+    Description : Places an element in the vector at the specified position
+*/
+
+#include <iostream>
+#include <vector>
+
+void print(const std::vector<int> &vec){
+	
+	for(auto num : vec){
+		std::cout << num << " ";
+	}
+}
+
+int main(){
+	//creating a vector of 5 elements
+	std::vector<int> vector1{10, 20, 30, 40, 50};
+
+	std::cout << "Initial vector: ";
+	print(vector1);
+	std::cout << std::endl;
+
+	//add an element at the beginning
+	vector1.emplace(vector1.begin(), -10);
+
+	//add an element at the end
+	vector1.emplace(vector1.end(), 60);
+
+	//add an element after 2 elements
+	vector1.emplace(vector1.begin() + 2, 15);
+
+	std::cout << "Modified vector: ";
+	print(vector1);
+	std::cout << std::endl;
+
+	return 0;
+}
