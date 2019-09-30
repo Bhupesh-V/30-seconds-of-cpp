8a1d0be302fb8ec4bb751ce1cc5b18306e6f7d23
Soumya Lahiri <say@soumyalahiri.com>
2019-10-01 00:30:49 +0530

    Added insert() in vector

--- snippets/vector/insert.cpp
+++ snippets/vector/insert.cpp
@@ -1,40 +1,23 @@
+// Style Guide => https://github.com/Bhupesh-V/30-Seconds-Of-STL/blob/master/CONTRIBUTING.md/#Style Guide
 /*
-    Author : Jordan Mingus
-    Date : 06/05/2019
-    Time : 03:50
-    Description : Showcase std::vector::insert to insert numbers into a vector.
-*/
+ Author : Soumya Lahiri
+ Date : 01/10/2019
+ Time : 01:00 AM
+ Description : insert() is a function in C++ STL which inserts new elements before the element at the specified position, effectively increasing the container size by the number of elements inserted.
+ */
+#include <bits/stdc++.h>
+int main() 
+{ 
+	// initialising the vector 
+	std::vector<int> vec = { 10, 20, 30, 40 }; 
 
-#include <iostream>
-#include <vector>
- 
-void print(const std::vector<int>& vec){
-    std::cout << '{';
-    for (auto x: vec) {
-        std::cout << ' ' << x;
-    }
-    std::cout << " }" << std::endl;
-}
- 
-int main (){
-    //Variables, change these to 
-    int insertPosition = 1; //Where in the vector it inser starting at 0.
-    int number = 42; //Number to be inserted into the vector.
-    int repeats = 3; //Number of time the number will be inserted into the vector.
-    
-    //setup example vector
-    std::vector<int> example = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    print(example);
-    
-    //insert with 2 inputs.
-    example.insert(example.begin() + insertPosition, number);
-    print(example);
-    
-    //reset example vector
-    example = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    print(example);
-    
-    //insert with 3 inputs.
-    example.insert(example.begin() + insertPosition, repeats, number);
-    print(example);
-}
+	// inserts 5 at front 
+	auto it = vec.insert(vec.begin(), 5);  
+
+	std::cout << "The vector elements are: "; 
+	for (auto it = vec.begin(); it != vec.end(); ++it) 
+		std::cout << *it << " "; 
+	std::cout << std::endl;
+
+	return 0; 
+} 
