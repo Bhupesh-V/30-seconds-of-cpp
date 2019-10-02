bba17db23cf96c291c7b7146eb52202bcd347b00
Rizky Agung <rizkyagung22@gmail.com>
2019-10-02 14:30:28 +0700

    add queue function

---
+++ queue/queue.md
@@ -0,0 +1,11 @@
+# queue
+
+**Description** : queue() function is used to constructs a queue container adaptor object.
+
+**Example**:
+```cpp
+    // Empty queue
+    std::queue<int> myqueue; 
+
+```
+**[Run Code](https://rextester.com/OEC31098)**
---
+++ snippets/queue/queue.cpp
@@ -0,0 +1,15 @@
+// Style Guide => https://github.com/Bhupesh-V/30-Seconds-Of-STL/blob/master/CONTRIBUTING.md/#Style Guide
+/*
+ Author : Rizky Agung
+ Date : 02/10/2019
+ Time : 02:27 PM
+ Description : queue() function is used to constructs a queue container adaptor object.
+ */
+#include <iostream>
+#include <queue>
+
+int main(){
+    // Empty queue
+    std::queue<int> myqueue;
+    
+}
