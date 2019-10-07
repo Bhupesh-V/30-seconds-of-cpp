74066cd4d8280615b36e5b2c7fa2d8eaa4bb8324
nishanthsanjeev <46843952+nishanthsanjeev@users.noreply.github.com>
2019-10-07 21:50:57 +0530

    Created __gcd.cpp

---
+++ snippets/algorithm/__gcd.cpp
@@ -0,0 +1,28 @@
+/*
+    Author : Nishanth Sanjeev
+    Date : 07/10/2019
+    Time : 21:45
+    Description : Finding gcd of two numbers, with only a function call
+*/
+
+
+#include<iostream>
+#include<algorithm>
+
+int main(){
+    
+    int num1 = 18;
+    int num2 = 4;
+    
+    // An example of finding gcd() of these two numbers.
+    // The answer returned should be 2, as 2 is the gcd of 18 and 4.
+    
+    std::cout << "GCD of num1 and num2 = " << __gcd(num1,num2) << endl;
+    
+    /*
+      As can be seen, we do not have to write Euclid's algorithm to find the gcd of two numbers. 
+      A simple function call works wonders! 
+    */
+
+    return 0;
+}
