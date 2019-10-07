7a860fd25aed1611d68eccc80cfc79c1eb23b07a
dennis <dennisongwenghan@gmail.com>
2019-10-06 19:32:47 -0700

    Added crbegin.cpp and crbegin.md

---
+++ snippets/vector/crbegin.cpp
@@ -0,0 +1,29 @@
+// Follow the Style Guide while submitting code PR.
+// Style Guide => https://github.com/Bhupesh-V/30-Seconds-Of-STL/blob/master/CONTRIBUTING.md/#Style Guide
+/*
+    Author : Dennis Weng Han Ong
+    Date : 6/10/2019
+    Time : 19:30
+    Description : vector function crbegin() is used to reverse vector iterations
+*/
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+int main (){ 
+    // initializing vector with values 
+    std::vector<int> vect = {10, 20, 30, 40, 50}; 
+  
+    // for loop with crbegin and crend to print results
+    
+    for (auto i = vect.crbegin(); i != vect.crend(); i++){ 
+    std::cout << ' ' << *i;  
+    }
+    
+    std:: cout << '\n'; 
+
+
+} 
---
+++ vector/crbegin.md
@@ -0,0 +1,16 @@
+# crbegin
+
+**Description** : It returns a const_reverse_iterator pointing to the last element in the container (i.e., its reverse beginning)..
+
+**Example**:
+```cpp
+    // declare the vector
+    std::vector<int> v = {10, 20, 30, 40, 50}; 
+
+    // calling function
+    std::v.crbegin();
+
+
+```
+**[See Sample code](../snippets/vector/crbegin.cpp)**
+**[Run Code](https://rextester.com/BBCF52794)**
