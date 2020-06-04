3a0fb1665f82f5b207b9f12dbcdf1331f7e610f3
Sarah Leon <51828255+YersiniaP@users.noreply.github.com>
2020-06-04 10:08:47 -0700

    Create set_symmetric_difference.cpp

---
+++ snippets/algorithm/set_symmetric_difference.cpp
@@ -0,0 +1,36 @@
+/*
+    Author : Sarah Leon
+    Date : 06/04/2020
+    Time : 08:20
+    Description : Compares two sets and fills a container with their difference.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+int main() {
+
+    //intialize arrays, must be sorted
+    int firstArray[5] = { 0, 2, 4, 6, 8 };
+    int secondArray[5] = { -4, -2 , 0, 2, 4 };
+
+    //destination array must be large enough to hold values
+    std::vector<int> destination(10);
+    std::vector<int>::iterator itr;
+
+    //set iterator to start at new vector of differences
+    itr = std::set_symmetric_difference(firstArray, firstArray + 5, secondArray, secondArray + 5, destination.begin());
+
+    //trim non-value elements
+    destination.resize(itr - destination.begin());
+
+    //print vector of values
+    std::cout << "The difference between the two sets is:";
+    for (int i = 0; i < destination.size(); ++i) {
+        std::cout << " " << destination[i];
+    }
+
+    return 0;
+}
