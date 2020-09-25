4ce7c4740a6cfc414e18ee99f564a1012096cacc
mourya <mouryachoubey@gmail.com>
2020-09-25 17:47:13 +0530

    Updated find_end.cpp

--- snippets/algorithm/find_end.cpp
+++ snippets/algorithm/find_end.cpp
@@ -10,17 +10,17 @@
 #include<algorithm>
 
 int main(){
-
     // first container
     std::vector<int> v{ 5, 3, 8, 5, 2, 9, 3, 8, 5 };
     // second container
-    std::vector<int> u{ 3, 8, 5 }
+    std::vector<int> u{ 3, 8, 5 };
 
     // find last occurence of vector u in vector v.
-    vector<int>::iterator it = std::find_end( v.begin(), v.end(), u.begin(), u.end() );
+    std::vector<int>::iterator i;
+    i = std::find_end( v.begin(), v.end(), u.begin(), u.end() );
 
     // displaying index of last occurence.
-    std::cout << "INDEX: " << ( it - v.begin() ) << '\n';
+    std::cout << "INDEX: " << ( i - v.begin() ) << '\n';
 
     return 0;
 }
