60e38055ffcfd21bba8a686b86d8cd5b327eb911
mourya <mouryachoubey@gmail.com>
2020-09-25 17:21:42 +0530

    Add code for find_end

---
+++ snippets/algorithm/find_end.cpp
@@ -0,0 +1,26 @@
+/*
+   Author : Mourya Choubey
+   Date : 25/09/2020
+   Time : 17:00
+   Description : Find last occurence of given container in another container.
+*/
+
+#include <iostream>
+#include<vector> 
+#include<algorithm>
+
+int main(){
+
+    // first container
+    std::vector<int> v{ 5, 3, 8, 5, 2, 9, 3, 8, 5 };
+    // second container
+    std::vector<int> u{ 3, 8, 5 }
+
+    // find last occurence of vector u in vector v.
+    vector<int>::iterator it = std::find_end( v.begin(), v.end(), u.begin(), u.end() );
+
+    // displaying index of last occurence.
+    std::cout << "INDEX: " << ( it - v.begin() ) << '\n';
+
+    return 0;
+}
