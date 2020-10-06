2f768f91e7bb1e37435e3e60a3e024a7fcb8a82c
umbridge <44002678+umbridge@users.noreply.github.com>
2020-10-06 17:24:37 +0530

    Update distance.cpp

--- snippets/algorithm/distance.cpp
+++ snippets/algorithm/distance.cpp
@@ -1,7 +1,7 @@
 /*
     Author : Apurva Dubey
-    Date : Date format 06/10/2020
-    Time : Time format 17:17
+    Date : 06/10/2020
+    Time : 17:17
     Description : returns the distance of desired position from the first iterator.
 */
 // demonstrate working of distance() 
@@ -15,10 +15,10 @@ int main()
 	// Initializing vector with array values 
 	int arr[] = {5, 10, 15, 20, 20, 23, 42, 45}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
-	vector<int> vect(arr, arr+n); 
+	std::vector<int> vect(arr, arr+n); 
 
 	// Return distance of first to maximum element 
-	cout << "Distance between first to max element: "; 
-	cout << distance(vect.begin(), max_element(vect.begin(), vect.end())); 
+	std::cout << "Distance between first to max element: "; 
+	std::cout << distance(vect.begin(), max_element(vect.begin(), vect.end())); 
 	return 0; 
 } 
