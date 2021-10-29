d0686f960795dc21ab231102258435257407c834
Rafael Silva <r4faelrs@gmail.com>
2021-10-29 11:39:18 -0300

    sintax fix

--- algorithm/heap_sort.cpp
+++ algorithm/heap_sort.cpp
@@ -2,12 +2,10 @@
 
 // C++ program for implementation of Heap Sort
 #include <iostream>
-using namespace std;
-  
+
 // To heapify a subtree rooted with node i which is
 // an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
-{
+void heapify(int arr[], int n, int i){
     int largest = i; // Initialize largest as root
     int l = 2 * i + 1; // left = 2*i + 1
     int r = 2 * i + 2; // right = 2*i + 2
@@ -22,7 +20,7 @@ void heapify(int arr[], int n, int i)
   
     // If largest is not root
     if (largest != i) {
-        swap(arr[i], arr[largest]);
+        std::swap(arr[i], arr[largest]);
   
         // Recursively heapify the affected sub-tree
         heapify(arr, n, largest);
@@ -30,8 +28,7 @@ void heapify(int arr[], int n, int i)
 }
   
 // main function to do heap sort
-void heapSort(int arr[], int n)
-{
+void heapSort(int arr[], int n){
     // Build heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
@@ -39,7 +36,7 @@ void heapSort(int arr[], int n)
     // One by one extract an element from heap
     for (int i = n - 1; i >= 0; i--) {
         // Move current root to end
-        swap(arr[0], arr[i]);
+        std::swap(arr[0], arr[i]);
   
         // call max heapify on the reduced heap
         heapify(arr, i, 0);
@@ -47,20 +44,18 @@ void heapSort(int arr[], int n)
 }
 
 /* A utility function to print array of size n */
-void printArray(int arr[], int n)
-{
+void printArray(int arr[], int n){
     for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << "\n";
+        std::cout << arr[i] << " ";
+    std::cout << "\n";
 }
   
-int main()
-{
+int main(){
     int arr[] = { 12, 11, 13, 5, 6, 7 }; // Define Array
     int n = sizeof(arr) / sizeof(arr[0]);
   
     heapSort(arr, n);
   
-    cout << "Sorted array is \n";
+    std::cout << "Sorted array is \n";
     printArray(arr, n);
 }
