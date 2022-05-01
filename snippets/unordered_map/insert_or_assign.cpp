aa81ee6c7d0724837946769f435c0496d2fd3faf
Blizzard Finnegan <blizzardfinnegan@gmail.com>
2022-05-01 16:26:19 -0400

    Add code snippet

---
+++ snippets/unordered_map/insert_or_assign.cpp
@@ -0,0 +1,22 @@
+// Demonstrates insert_or_assign()
+#include <iostream>
+#include <unordered_map>
+
+int main(){
+    //declares an empty map. O(1)
+    std::unordered_map<char, int> mymap;
+
+    // inserting in to unordered_map with O(1) time on average
+    // NOTE: Unlike insert(), this is not an overloaded function,
+    // so it takes in two arguments rather than one
+    mymap.insert_or_assign('A', 1);
+    mymap.insert_or_assign('b', 2);
+    mymap.insert_or_assign('c', 3);
+    mymap.insert_or_assign('A', 4);
+
+    //print unordered_map elements
+    for (auto it = mymap.begin(); it != mymap.end(); ++it){
+        std::cout << it->first << " = "<< it->second << '\n';
+    }
+    return 0;
+}
