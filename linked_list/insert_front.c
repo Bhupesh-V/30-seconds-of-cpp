cb9c876feb50fd939218e71662a8c8afaa67fca4
Khudd1214 <77385940+Khudd1214@users.noreply.github.com>
2022-06-07 14:28:31 -0700

    Create insert_front.c
    A demonstration of the algorithm for 'pushing' values to the front of a linked list.

---
+++ linked_list/insert_front.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//Singly Linked Node implementation storing integer values
+struct SLNode{
+    int val;
+    struct SLNode* next;
+};
+
+//traverse list and print values
+void print_list(struct SLNode* n){
+  while (n != NULL){
+    printf("%i ", n -> val);
+    n = n -> next;
+  }
+  printf("\n");
+}
+
+//insert values at front of list, return the updated list. List must have at least one node defined.
+struct SLNode* insert_front(int newVal, struct SLNode* head){
+    struct SLNode* new_node = NULL;
+    new_node = (struct SLNode*)malloc(sizeof(struct SLNode));
+    //update val of new_node
+    new_node -> val = newVal;
+    //update new_node to point to current head node
+    new_node -> next = head;
+
+    //return new_node, so 'head' in calling function can be updated
+    return new_node;
+}
+
+int main() {
+    struct SLNode* list = NULL;
+    //define first node of 'list'
+    list = (struct SLNode*)malloc(sizeof(struct SLNode));
+    list -> val = 3;
+    list -> next = NULL;
+    //insert values 1 and 9 to 'list'
+    list = insert_front(1, list);
+    list = insert_front(9, list);
+    print_list(list);
+    return 0;
+}
