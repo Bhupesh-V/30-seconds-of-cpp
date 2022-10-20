86dc04f139ca6d0389d71c3da2706a84eb6543d1
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2022-10-20 10:41:46 +0530

    Create permutation-string.cpp

---
+++ permutation-string.cpp
@@ -0,0 +1,44 @@
+// C++ program to print all
+// permutations with duplicates allowed
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Function to print permutations of string
+// This function takes three parameters:
+// 1. String
+// 2. Starting index of the string
+// 3. Ending index of the string.
+void permute(string a, int l, int r)
+{
+	// Base case
+	if (l == r)
+		cout<<a<<endl;
+	else
+	{
+		// Permutations made
+		for (int i = l; i <= r; i++)
+		{
+
+			// Swapping done
+			swap(a[l], a[i]);
+
+			// Recursion called
+			permute(a, l+1, r);
+
+			//backtrack
+			swap(a[l], a[i]);
+		}
+	}
+}
+
+// Driver Code
+int main()
+{
+	string str = "ABC";
+	int n = str.size();
+	permute(str, 0, n-1);
+	return 0;
+}
+
+// This is code is contributed by rathbhupendra
